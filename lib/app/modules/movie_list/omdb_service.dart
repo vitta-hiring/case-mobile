5a02b2e189a69ecb990a27f1b567e9e00359df9e
Murilo Oliveira <murilo.ao@hotmail.com>
2020-07-28 22:54:23 -0300

    chore(service): Implementation of OMDBService

---
+++ lib/app/modules/movie_list/omdb_service.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:dio/dio.dart';
+import 'package:vitta_test/app/helpers/http_helper.dart';
+import 'package:vitta_test/app/modules/movie_list/full_movie_model.dart';
+import 'package:vitta_test/app/modules/movie_list/movie_model.dart';
+import 'package:vitta_test/app/utils/settings.dart';
+
+const OMDB_MOVIE_SEARCH_TYPE = 'movie';
+const OMDB_SERIES_SEARCH_TYPE = 'series';
+const OMDB_EPISODE_SEARCH_TYPE = 'episode';
+
+class OMDBService extends Disposable {
+  final HttpHelper _http = HttpHelper(OMDB_API_URL, OMDB_API_TIMEOUT);
+
+  Future<List<MovieModel>> _getMidiaByTitleAndType(
+      String title, String searchtype) async {
+    List<MovieModel> movies = [];
+    Response response = await this._http.get(queryParameters: {
+      's': title,
+      'apikey': OMDB_API_KEY,
+      'type': searchtype,
+    });
+    if (response.statusCode == HttpStatus.ok) {
+      ((response.data['Search'] ?? []) as List)
+          .forEach((movie) => movies.add(MovieModel.fromJson(movie)));
+    } else if (response.statusCode == HttpStatus.unauthorized) {
+      print('Invalid API key');
+      throw Exception('Invalid API key');
+    }
+    return movies;
+  }
+
+  Future<List<MovieModel>> getMoviesByTitle(String title) async {
+    return this._getMidiaByTitleAndType(title, OMDB_MOVIE_SEARCH_TYPE);
+  }
+
+  Future<List<MovieModel>> getSeriesByTitle(String title) async {
+    return this._getMidiaByTitleAndType(title, OMDB_SERIES_SEARCH_TYPE);
+  }
+
+  Future<List<MovieModel>> getEpisodesByTitle(String title) async {
+    return this._getMidiaByTitleAndType(title, OMDB_EPISODE_SEARCH_TYPE);
+  }
+
+  Future<FullMovieModel> getMidiaById(String midiaId) async {
+    Response response = await this._http.get(queryParameters: {
+      'i': midiaId,
+      'apikey': OMDB_API_KEY,
+    });
+    if (response.statusCode == HttpStatus.ok &&
+        response.data['Response'] == "True") {
+      return FullMovieModel.fromJson(response.data);
+    } else if (response.statusCode == HttpStatus.unauthorized) {
+      print('Invalid API key');
+      throw Exception('Invalid API key');
+    }
+    return null;
+  }
+
+  //dispose will be called automatically
+  @override
+  void dispose() {
+    print('executando nos ervicoe');
+    this._http.dispose();
+    print('terminou noservice');
+  }
+}
---
+++ lib/app/utils/settings.dart
@@ -0,0 +1,4 @@
+const APP_NAME = "Vitta Test";
+const OMDB_API_URL = "http://www.omdbapi.com";
+const OMDB_API_KEY = "d12b4be8";
+const OMDB_API_TIMEOUT = 8000;
