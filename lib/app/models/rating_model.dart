a1b5349ffcb5f37279113953c154ca17baa4b3a3
Murilo Oliveira <murilo.ao@hotmail.com>
2020-07-30 23:52:31 -0300

    chore(model): Adding rating constants to model

--- lib/app/models/rating_model.dart
+++ lib/app/models/rating_model.dart
@@ -1,3 +1,9 @@
+import 'package:vitta_test/app/utils/resources.dart';
+
+const IMDB = 'Internet Movie Database';
+const METACRITIC = 'Metacritic';
+const ROTTEN_TOMATOES = 'Rotten Tomatoes';
+
 class RatingModel {
   RatingModel({
     this.source,
@@ -16,4 +22,13 @@ class RatingModel {
         "Source": source,
         "Value": value,
       };
+
+  static String getPathIcon(String name) {
+    if (name == IMDB)
+      return IMDB_ICON_PATH;
+    else if (name == METACRITIC)
+      return METACRITIC_ICON_PATH;
+    else if (name == ROTTEN_TOMATOES) return ROTTEN_TOMATOES_ICON_PATH;
+    return null;
+  }
 }
